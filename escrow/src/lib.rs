1edbd325f30390bbeea382b4eeb3d8187ddd0b93
Daniel <daniel.hummelstad@gmail.com>
2022-05-22 23:43:19 +1000

    Added withdraw from escrow account

--- escrow/src/lib.rs
+++ escrow/src/lib.rs
@@ -1,10 +1,12 @@
 use std::cell::RefCell;
+use std::convert::TryInto;
 
 use candid::{candid_method, export_service, Nat, Principal};
 use ic_cdk::caller;
 use ic_cdk_macros::*;
 use ic_ledger_types::{
-    AccountIdentifier, Memo, Tokens, DEFAULT_SUBACCOUNT, MAINNET_LEDGER_CANISTER_ID,
+    AccountBalanceArgs, AccountIdentifier, Memo, Tokens, DEFAULT_SUBACCOUNT,
+    MAINNET_LEDGER_CANISTER_ID,
 };
 
 mod types;
@@ -17,6 +19,13 @@ const ICP_FEE: u64 = 10_000;
 
 // https://github.com/dfinity/examples/blob/master/motoko/defi/architecture.md
 
+// Need to think about the signature flow. Look at some other applications
+// just use principals for now. They seem to be unique per user.
+
+// so have a func which is authorize(). this saves to the state that caller() x is authorizing
+// the escrow.
+// need to have a check to push back transactions if the cycles are close to running out on the canister
+
 thread_local! {
     static STATE: RefCell<State> = RefCell::new(State::default());
 }
@@ -24,7 +33,7 @@ thread_local! {
 #[derive(Default)]
 pub struct State {
     owner: Option<Principal>,
-    ledger: Option<Principal>,
+    ledger: Option<Principal>, // IC ledger
 }
 
 #[init]
@@ -37,7 +46,7 @@ fn init(ledger: Option<Principal>) {
 }
 
 #[update]
-pub async fn deposit(_token_canister_id: Option<Principal>) -> DepositReceipt {
+pub async fn deposit(_token_canister_id: Principal) -> DepositReceipt {
     let caller = caller();
     let ledger_canister_id = STATE
         .with(|s| s.borrow().ledger)
@@ -75,10 +84,111 @@ pub fn get_deposit_address() -> AccountIdentifier {
     AccountIdentifier::new(&canister_id, &subaccount)
 }
 
+#[update(name = "withdraw")]
+pub async fn withdraw(
+    amount: Nat,
+    _token_canister_id: Principal,
+    address: Principal,
+) -> WithdrawReceipt {
+    let caller = caller();
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+
+    if _token_canister_id == ledger_canister_id {
+        let account_id = AccountIdentifier::new(&address, &DEFAULT_SUBACCOUNT);
+        withdraw_icp(&amount, account_id).await
+    } else {
+        ic_cdk::print("token");
+        return Err(WithdrawErr::TransferFailure);
+        // withdraw_token(token_canister_id, &amount, address).await
+    }
+}
+
+async fn withdraw_icp(amount: &Nat, account_id: AccountIdentifier) -> Result<Nat, WithdrawErr> {
+    let caller = caller();
+    let canister_id = ic_cdk::api::id();
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+
+    let escrow_account = AccountIdentifier::new(&canister_id, &principal_to_subaccount(&caller));
+    let transfer_amount = Tokens::from_e8s(
+        (amount.to_owned() + ICP_FEE)
+            .0
+            .try_into()
+            .map_err(|_| WithdrawErr::TransferFailure)?,
+    );
+    let balance = ic_ledger_types::account_balance(
+        ledger_canister_id,
+        AccountBalanceArgs {
+            account: escrow_account,
+        },
+    )
+    .await
+    .map_err(|_| WithdrawErr::TransferFailure)?;
+    if balance < transfer_amount {
+        return Err(WithdrawErr::BalanceLow);
+    }
+
+    let transfer_args = ic_ledger_types::TransferArgs {
+        memo: Memo(0),
+        amount: transfer_amount,
+        fee: Tokens::from_e8s(ICP_FEE),
+        from_subaccount: Some(DEFAULT_SUBACCOUNT),
+        to: account_id,
+        created_at_time: None,
+    };
+    let icp_reciept = ic_ledger_types::transfer(ledger_canister_id, transfer_args)
+        .await
+        .map_err(|_| WithdrawErr::TransferFailure)
+        .and_then(|v| v.map_err(|_| WithdrawErr::TransferFailure));
+
+    ic_cdk::println!("Withdrawal of {} ICP to account {:?}", amount, &account_id);
+
+    Ok(amount.to_owned() + ICP_FEE)
+}
+
+async fn get_escrow_balance(account: Principal) -> Result<Balance, GeneralErr> {
+    let canister_id = ic_cdk::api::id();
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+
+    let escrow_account = AccountIdentifier::new(&canister_id, &principal_to_subaccount(&account));
+    let balance = ic_ledger_types::account_balance(
+        ledger_canister_id,
+        AccountBalanceArgs {
+            account: escrow_account,
+        },
+    )
+    .await
+    .map_err(|_| GeneralErr::CallFailure)?;
+
+    Ok(Balance {
+        owner: account,
+        token: ledger_canister_id,
+        amount: balance.e8s().into(),
+    })
+}
+
 #[update]
 pub fn string_address() -> String {
     let canister_id = ic_cdk::api::id();
     let subaccount = principal_to_subaccount(&caller());
 
     AccountIdentifier::new(&canister_id, &subaccount).to_string()
+}
+
+#[query]
+pub fn whoami() -> String {
+    caller().to_text()
+}
+
+#[query]
+pub fn ledger() -> String {
+    let ledger_canister_id = STATE
+        .with(|s| s.borrow().ledger)
+        .unwrap_or(MAINNET_LEDGER_CANISTER_ID);
+    ledger_canister_id.to_text()
 }
--- escrow/src/types.rs
+++ escrow/src/types.rs
@@ -2,8 +2,28 @@ use candid::{CandidType, Nat, Principal};
 
 pub type DepositReceipt = Result<Nat, DepositErr>;
 
+pub type WithdrawReceipt = Result<Nat, WithdrawErr>;
+
+#[derive(CandidType)]
+pub struct Balance {
+    pub owner: Principal,
+    pub token: Principal,
+    pub amount: Nat,
+}
+
 #[derive(CandidType)]
 pub enum DepositErr {
     BalanceLow,
     TransferFailure,
+}
+
+#[derive(CandidType)]
+pub enum WithdrawErr {
+    BalanceLow,
+    TransferFailure,
+}
+
+#[derive(CandidType)]
+pub enum GeneralErr {
+    CallFailure,
 }
